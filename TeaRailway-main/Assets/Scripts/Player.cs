ab70073efa05357aa5f3b716a8139c5d40dd3615
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2024-07-05 17:56:16 +0900

    葉っぱに触れると加速

--- TeaRailway-main/Assets/Scripts/Player.cs
+++ TeaRailway-main/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class Player : MonoBehaviour
     [SerializeField]
     [Tooltip("ジャンプ力を指定します。")]
     private Vector2 jumpPower = new Vector2(0, 6);
+
+    [SerializeField]
+    [Tooltip("ジャンプ力を指定します。")]
+    private float dashPower = 4.0f;
+
+    private float deltaSpeed = 0f;
 
     private Rigidbody rigidbody;
 
@@ -26,8 +33,30 @@ public class Player : MonoBehaviour
             rigidbody.AddForce(jumpPower, ForceMode.Impulse);
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            deltaSpeed = dashPower;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            deltaSpeed = dashPower -2;
+        }
+
+
+
         var velocity = rigidbody.velocity;
-        velocity.x = speed;
+        velocity.x = speed + deltaSpeed;
         rigidbody.velocity = velocity;
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GreenLeaf"))
+        {
+            Debug.Log("GreenLeaf");
+            deltaSpeed = dashPower;
+        }
+    }
+
 }
