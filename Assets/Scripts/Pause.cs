d83443675a085731d15974699a07504963344a07
Ueno Shingo <ueno_shingo2002@yahoo.co.jp>
2024-07-12 20:38:01 +0900

    ポーズ状況の作成

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static Player;
 
 public class Pause : MonoBehaviour
 {
     bool PauseFlag = false;
+    [SerializeField] GameObject PauseUI;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PauseUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,12 +23,22 @@ public class Pause : MonoBehaviour
             {
                 PauseFlag = true;
                 Time.timeScale = 0;
+                PauseUI.SetActive(true);
             }
             else
             {
                 PauseFlag = false;
                 Time.timeScale = 1f;
+                PauseUI.SetActive(false);
             }
         }
     }
+
+    public void RestartButton()
+    {
+        Debug.Log("リスタート");
+        SceneManager.LoadScene("Stage1");
+    }
+
+
 }
